1594e917b562bd69f86775d7009665872e9c0980
FujiHaruka <fizzyjohn1427@gmail.com>
2022-10-22 15:18:44 +0900

    Copy example/standalone.js from panva/node-oidc-provider

---
+++ app.js
@@ -0,0 +1,77 @@
+// https://github.com/panva/node-oidc-provider
+// MIT License
+const path = require("path");
+const { promisify } = require("util");
+
+const render = require("@koa/ejs");
+const helmet = require("helmet");
+
+const { Provider } = require("oidc-provider");
+
+const Account = require("./src/account");
+const configuration = require("./src/configuration");
+const routes = require("./src/routes");
+
+const { PORT = 3000, ISSUER = `http://localhost:${PORT}` } = process.env;
+configuration.findAccount = Account.findAccount;
+
+let server;
+
+(async () => {
+  const prod = process.env.NODE_ENV === "production";
+
+  const provider = new Provider(ISSUER, { ...configuration });
+
+  const directives = helmet.contentSecurityPolicy.getDefaultDirectives();
+  delete directives["form-action"];
+  const pHelmet = promisify(
+    helmet({
+      contentSecurityPolicy: {
+        useDefaults: false,
+        directives,
+      },
+    })
+  );
+
+  provider.use(async (ctx, next) => {
+    const origSecure = ctx.req.secure;
+    ctx.req.secure = ctx.request.secure;
+    await pHelmet(ctx.req, ctx.res);
+    ctx.req.secure = origSecure;
+    return next();
+  });
+
+  if (prod) {
+    provider.proxy = true;
+    provider.use(async (ctx, next) => {
+      if (ctx.secure) {
+        await next();
+      } else if (ctx.method === "GET" || ctx.method === "HEAD") {
+        ctx.status = 303;
+        ctx.redirect(ctx.href.replace(/^http:\/\//i, "https://"));
+      } else {
+        ctx.body = {
+          error: "invalid_request",
+          error_description: "do yourself a favor and only use https",
+        };
+        ctx.status = 400;
+      }
+    });
+  }
+  render(provider.app, {
+    cache: false,
+    viewExt: "ejs",
+    layout: "_layout",
+    root: path.join(__dirname, "views"),
+  });
+  provider.use(routes(provider).routes());
+  server = provider.listen(PORT, () => {
+    console.log(
+      `application is listening on port ${PORT}, check its /.well-known/openid-configuration`
+    );
+  });
+})().catch((err) => {
+  if (server && server.listening) server.close();
+  console.error(err);
+  process.exitCode = 1;
+});
