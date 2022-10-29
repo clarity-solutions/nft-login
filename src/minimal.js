1877b809f28c8b563dbbf6fa10ff5508e923dab1
FujiHaruka <fizzyjohn1427@gmail.com>
2022-10-29 09:49:48 +0900

    format

--- src/minimal.js
+++ src/minimal.js
@@ -1,20 +1,20 @@
-const Provider = require('oidc-provider');
+const Provider = require("oidc-provider");
 
 // new Provider instance with no extra configuration, will run in default, just needs the issuer
 // identifier, uses data from runtime-dyno-metadata heroku here
 const oidc = new Provider("http://localhost:3000", {
   clients: [
     {
-      client_id: 'example_client',
-      client_secret: 'mmake95#kDuRRRR#3rak3r1dccaMd',
+      client_id: "example_client",
+      client_secret: "mmake95#kDuRRRR#3rak3r1dccaMd",
       redirect_uris: [
-        'https://example.localhost/callback',
+        "https://example.localhost/callback",
         // using jwt.io as redirect_uri to show the ID Token contents
-        'https://jwt.io',
+        "https://jwt.io",
       ],
-      response_types: ['id_token'],
-      grant_types: ['implicit'],
-      token_endpoint_auth_method: 'none',
+      response_types: ["id_token"],
+      grant_types: ["implicit"],
+      token_endpoint_auth_method: "none",
     },
   ],
   cookies: {
